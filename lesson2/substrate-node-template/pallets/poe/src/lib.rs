34954f9b91bd2460f9bb94bdf74215c3eba57d14
kenny <19022095@qq.com>
2020-06-06 17:37:39 +0800

    lesson 2

---
+++ lesson2/substrate-node-template/pallets/poe/src/lib.rs
@@ -0,0 +1,91 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+
+
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, ensure};
+use frame_system::{self as system, ensure_signed};
+use sp_std::prelude::*;
+
+// #[cfg(test)]
+// mod mock;
+
+// #[cfg(test)]
+// mod tests;
+
+pub trait Trait: system::Trait {
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as TemplateModule {
+		Proofs get(fn proofs): map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
+	}
+}
+
+decl_event!(
+	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
+		ClaimCreated(AccountId, Vec<u8>),
+		ClaimRevoked(AccountId, Vec<u8>),
+		ClaimTransfered(AccountId, AccountId, Vec<u8>),
+	}
+);
+
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		NoneValue,
+		StorageOverflow,
+		ClaimNotExist,
+		ClaimRevoked,
+		ProofAlreadyExist,
+		NotClaim,
+		ClaimTooLong,
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		type Error = Error<T>;
+
+		fn deposit_event() = default;
+
+		#[weight = 0]
+		pub fn create_claim(origin, claim: Vec<u8>) -> dispatch::DispatchResult{
+			let claim_limit: usize = 8;
+			ensure!(&claim.len()<=&claim_limit, Error::<T>::ClaimTooLong);
+
+			let sender = ensure_signed(origin)?;
+			ensure!(!Proofs::<T>::contains_key(&claim), Error::<T>::ProofAlreadyExist);
+
+			Proofs::<T>::insert(&claim, (sender.clone(), system::Module::<T>::block_number()));
+			Self::deposit_event(RawEvent::ClaimCreated(sender, claim));
+			
+			 Ok(())
+		}
+
+		#[weight = 0]
+		pub fn revoke_claim(origin, claim: Vec<u8>) -> dispatch::DispatchResult{
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Proofs::<T>::contains_key(&claim), Error::<T>::ClaimNotExist);
+
+			let (owner, _block_number) = Proofs::<T>::get(&claim);
+			ensure!(owner == sender, Error::<T>::NotClaim);
+			Proofs::<T>::remove(&claim);
+			Self::deposit_event(RawEvent::ClaimRevoked(sender, claim));
+			Ok(())
+		}
+
+		#[weight = 0]
+		pub fn transfer_claim(origin, _to: T::AccountId, claim: Vec<u8>) -> dispatch::DispatchResult{
+			let sender = ensure_signed(origin)?;
+			ensure!(Proofs::<T>::contains_key(&claim), Error::<T>::ClaimNotExist);
+			
+			let (owner, _block_number) = Proofs::<T>::get(&claim);
+			ensure!(owner == sender, Error::<T>::NotClaim);
+
+			Proofs::<T>::insert(&claim, (_to.clone(), system::Module::<T>::block_number()));
+			Self::deposit_event(RawEvent::ClaimTransfered(sender, _to, claim));
+			Ok(())
+		}
+	}
+}
