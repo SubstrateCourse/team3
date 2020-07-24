f76770435b2eb5e2363a354915285c262abefc0e
zhang chun <ftexmrealtime@gmail.com>
2020-07-24 18:11:22 +0800

    pic lesson

--- lesson10/erc20/lib.rs
+++ lesson10/erc20/lib.rs
@@ -124,12 +124,8 @@ mod erc20 {
         }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
         #[test]
